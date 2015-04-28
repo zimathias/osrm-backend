cd84ba2432fddb6b5f507a86e4a4689d48ba97f3
Lauren Budorick <lauren@mapbox.com>
2015-04-28 15:30:32 -0400

    bootstrap.sh @ first draft

---
+++ bootstrap.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+function dep() {
+    ./.mason/mason install $1 $2
+    ./.mason/mason link $1 $2
+}
+
+# default to clang
+CXX=${CXX:-clang++}
+
+function all_deps() {
+    dep cmake 3.2.2 &
+    dep lua 5.3.0 &
+    dep luabind dev &
+    dep boost 1.57.0 &
+    dep boost_libsystem 1.57.0 &
+    dep boost_libthread 1.57.0 &
+    dep boost_libfilesystem 1.57.0 &
+    dep boost_libprogram_options 1.57.0 &
+    dep boost_libregex 1.57.0 &
+    dep boost_libiostreams 1.57.0 &
+    dep boost_libtest 1.57.0 &
+    dep boost_libdate_time 1.57.0 &
+    wait
+}
+
+function main() {
+    if [[ ! -d ./.mason ]]; then
+        git clone --depth 1 https://github.com/mapbox/mason.git ./.mason
+    fi
+    export MASON_DIR=$(pwd)/.mason
+    export MASON_HOME=$(pwd)/mason_packages/.link
+    if [[ ! -d ${MASON_HOME} ]]; then
+        all_deps
+    fi
+    export PATH=${MASON_HOME}/bin:$PATH
+    export PKG_CONFIG_PATH=${MASON_HOME}/lib/pkgconfig
+
+    # environment variables to tell the compiler and linker
+    # to prefer mason paths over other paths when finding
+    # headers and libraries. This should allow the build to
+    # work even when conflicting versions of dependencies
+    # exist on global paths
+    # stopgap until c++17 :) (http://www.open-std.org/JTC1/SC22/WG21/docs/papers/2014/n4214.pdf)
+    export C_INCLUDE_PATH="${MASON_HOME}/include"
+    export CPLUS_INCLUDE_PATH="${MASON_HOME}/include"
+    export LIBRARY_PATH="${MASON_HOME}/lib"
+
+    LINK_FLAGS=""
+    LDFLAGS=""
+    if [[ $(uname -s) == 'Linux' ]]; then
+        LINK_FLAGS="${LINK_FLAGS} "'-Wl,-z,origin -Wl,-rpath=\$ORIGIN'
+        export LDFLAGS="${LINK_FLAGS}"
+    fi
+
+    rm -rf build
+    mkdir -p build
+    cd build
+    cmake ../ -DCMAKE_INSTALL_PREFIX=${MASON_HOME} \
+      -DCMAKE_CXX_COMPILER="$CXX" \
+      -DBoost_NO_SYSTEM_PATHS=ON \
+      -DTBB_INSTALL_DIR=${MASON_HOME} \
+      -DCMAKE_INCLUDE_PATH=${MASON_HOME}/include \
+      -DCMAKE_LIBRARY_PATH=${MASON_HOME}/lib \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_EXE_LINKER_FLAGS="${LINK_FLAGS}"
+    make -j${JOBS}
+    make install
+
+    cd ../
+    echo "success"
+}
+
+main
