59f6e697672b41ed02b8c6bc50225ab6cc43cc19
Dane Springmeyer <dane@mapbox.com>
2015-05-01 21:49:42 -0700

    centralize osmosis install further

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,13 +8,12 @@ mkdir -p build
 cd build
 if [[ ${CMAKEOPTIONS} == false ]]; then
     cmake ../ -DCMAKE_INSTALL_PREFIX=${MASON_HOME} \
-        -DCMAKE_CXX_COMPILER="$CXX" \
+        -DCMAKE_CXX_COMPILER="${CXX}" \
         -DBoost_NO_SYSTEM_PATHS=ON \
         -DTBB_INSTALL_DIR=${MASON_HOME} \
         -DCMAKE_INCLUDE_PATH=${MASON_HOME}/include \
         -DCMAKE_LIBRARY_PATH=${MASON_HOME}/lib \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_EXE_LINKER_FLAGS="${LINK_FLAGS}"
+        -DCMAKE_BUILD_TYPE=Release
 else
     cmake ../ ${CMAKEOPTIONS} -DBUILD_TOOLS=1
 fi
