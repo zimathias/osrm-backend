43826b5df28bfb754be423bd1c5425494d492274
Dane Springmeyer <dane@mapbox.com>
2015-05-01 19:45:33 -0700

    simplify by adding coverage to mason-install

--- scripts/MASON-install.sh
+++ scripts/MASON-install.sh
@@ -2,12 +2,19 @@
 
 set -o pipefail
 
-if [[ $(uname -s) == 'Linux' ]]; then
-    export CXX="g++-4.8"
-    export CC="gcc-4.8"
-    export PYTHONPATH=$(pwd)/mason_packages/.link/lib/python2.7/site-packages
-else
-    export PYTHONPATH=$(pwd)/mason_packages/.link/lib/python/site-packages
-fi
-
 source ./bootstrap.sh
+
+if [[ ${COVERAGE} == true ]];
+    PYTHONUSERBASE=$(pwd)/mason_packages/.link pip install --user cpp-coveralls;
+    export LDFLAGS="--coverage"
+    export CXXFLAGS="--coverage"
+    if [[ $(uname -s) == 'Linux' ]]; then
+        # TODO: figure out how to avoid sudo
+        sudo apt-get -q install rubygems
+        export CXX="g++-4.8"
+        export CC="gcc-4.8"
+        export PYTHONPATH=$(pwd)/mason_packages/.link/lib/python2.7/site-packages
+    else
+        export PYTHONPATH=$(pwd)/mason_packages/.link/lib/python/site-packages
+    fi
+fi
