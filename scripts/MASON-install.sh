0ca58d58d68162d87ba38b7a06badae75709b54c
Dane Springmeyer <dane@mapbox.com>
2015-05-01 22:02:48 -0700

    fix setting of CXX/CC to c++11 capable compiler

--- scripts/MASON-install.sh
+++ scripts/MASON-install.sh
@@ -4,15 +4,18 @@ set -o pipefail
 
 source ./bootstrap.sh
 
+if [[ $(uname -s) == 'Linux' ]]; then
+    # TODO: figure out how to avoid sudo
+    sudo apt-get -q install rubygems
+    export CXX="g++-4.8"
+    export CC="gcc-4.8"
+fi
+
 if [[ ${COVERAGE} == true ]]; then
     PYTHONUSERBASE=$(pwd)/mason_packages/.link pip install --user cpp-coveralls;
     export LDFLAGS="--coverage"
     export CXXFLAGS="--coverage"
     if [[ $(uname -s) == 'Linux' ]]; then
-        # TODO: figure out how to avoid sudo
-        sudo apt-get -q install rubygems
-        export CXX="g++-4.8"
-        export CC="gcc-4.8"
         export PYTHONPATH=$(pwd)/mason_packages/.link/lib/python2.7/site-packages
     else
         export PYTHONPATH=$(pwd)/mason_packages/.link/lib/python/site-packages
